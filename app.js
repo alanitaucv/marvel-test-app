390187a0eed1cdcd577e6b069abb24d47f8d4e4b
zileplus <alanitaucv@gmail.com>
2017-06-02 16:23:50 -0400

    Conexion exitosa a API MARVEL

--- app.js
+++ app.js
@@ -21,12 +21,16 @@
       $rootScope.port = '443';
       $rootScope.basePath = '/v1/public/';
       $rootScope.url = $rootScope.host + ':' + $rootScope.port+$rootScope.basePath;
-      $rootScope.apiKey = 'aquiVaTuApiKeyPublica';
-      $rootScope.apiKeyP= 'aquiVaTuApiKeyPrivada';
+      $rootScope.url1 = 'http://gateway.marvel.com/v1/public/';
+      $rootScope.apiKey = '64b478c2ce8591ad0d3598e18352bb5c';
+      $rootScope.apiKeyP= '70fdd466e51bc7197ca0cd8a0a7cc5694a5a222d';
       $rootScope.limit = 10;
       $rootScope.headers =  {
         "Content-Type" : "application/json",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        'Authorization':'Basic YWxhbml0YXVjdkBnbWFpbC5jb206WmFzbzE5ODQq',
+         'Accept': '*/*',
+         'Access-Control-Allow-Origin': '*'
       };
   }]);
 
@@ -38,18 +42,24 @@
       var url = $rootScope.url + 'characters?offset='+skip+'&limit='+$rootScope.limit+'&apikey='+$rootScope.apiKey;
       var hash = CryptoJS.MD5(ts + $rootScope.apiKeyP + $rootScope.apiKey, 'hex');
       url += "&ts="+ts+"&hash="+hash;
-
       console.log(url);
-
+      url = 'https://gateway.marvel.com/v1/public/characters?offset=0&limit=10&apikey=64b478c2ce8591ad0d3598e18352bb5c&ts=1496282040288&hash=25cccf0ff4013fde152a82a1cf69edf9';
       $http({
          method: 'GET',
          headers: $rootScope.headers,
          url: url
-      }).then(function (data, status, success){
-          console.log('Paso'+angular.toJson(data));
-      },function (data, status, error){
+      }).then(function (success){
+          console.log('Paso');
+      },function (error){
           console.log("Error consumiendo el servicio");
       });
+      // $http.get(url, $rootScope.headers)
+      //   .success( function (data, status, headers, config){
+      //     console.log('Paso');
+      //   })
+      //   .error( function (data, status, headers, config){
+      //     console.log("Error consumiendo el servicio");
+      //   });
     };
 
 	}]);
